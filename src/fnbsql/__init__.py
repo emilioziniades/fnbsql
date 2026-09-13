364e8d895a48e8c327e6a48a73b257cf672913a3
Emilio (Racecar) Ziniades <emilioziniades@protonmail.com>
2026-09-13 17:24:15 +0200

    first pass at parsing pdfs

--- src/fnbsql/__init__.py
+++ src/fnbsql/__init__.py
@@ -1,2 +1,29 @@
-def main() -> None:
-    print("Hello from fnbsql!")
+from argparse import ArgumentParser
+from pprint import pprint
+
+import pdfplumber
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", action="append", required=True)
+    args = parser.parse_args()
+
+    files = args.file
+
+    print(args.file)
+    for file in files:
+        with pdfplumber.open(file) as pdf:
+            for page in pdf.pages:
+                table = page.extract_tables(
+                    {
+                        "vertical_strategy": "lines",
+                        "horizontal_strategy": "text",
+                        "min_words_horizontal": 6,
+                        "snap_x_tolerance": 4,
+                        "snap_y_tolerance": 4,
+                    }
+                )
+                pprint(table)
+                breakpoint()
+                break
